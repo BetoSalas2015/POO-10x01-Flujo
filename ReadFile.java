abb1b7a0c4fd2d70cb4544d413014a462749b320
BetoSalas2015 <roberto.salazar@upaep.mx>
2020-05-11 17:28:03 -0500

    Lectura de caracteres de 1 Byte

---
+++ ReadFile.java
@@ -0,0 +1,56 @@
+/**
+ * Write a description of class ReadFile here.
+ * 
+ * @author Roberto Salazar Márquez
+ * @version 1.0
+ */
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class ReadFile extends Frame
+{
+    // instance variables - replace the example below with your own
+    private TextArea editor;
+    private FileInputStream entrada;
+
+    /**
+     * Constructor for objects of class ReadFile
+     */
+    public ReadFile() throws IOException
+    {
+        // initialise instance variables
+        super("Lector de Archivos");
+        int c;
+        editor = new TextArea();
+        add(editor, "Center");
+        
+        try {
+            entrada = new FileInputStream("ReadFile.java");
+            
+            while( (c = entrada.read()) != -1) 
+                editor.append( String.valueOf((char) c ));
+        }catch (IOException e) {
+              e.printStackTrace(); }
+        finally {
+            if(entrada != null) 
+                entrada.close();
+            }
+        
+        addWindowListener(new CW());
+        setSize(350,400);
+        setVisible(true);
+    }
+    
+    private class CW extends WindowAdapter
+    {
+        public void windowClosing(WindowEvent e)
+        {
+            setVisible(false);
+            dispose();
+            System.exit(0);
+        }
+    }
+
+}
+
